5472b0d9d78fa210dca3af6b3cfbbaf179f486db
tiwary1998prashant <tiwaryprashant59@gmail.com>
2026-03-25 00:45:48 +0530

    auth progress

---
+++ auth-service/src/main/java/com/jitlabs/authservice/config/JwtUtil.java
@@ -0,0 +1,28 @@
+package com.jitlabs.authservice.config;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    private final String SECRET = "my-secret-key";
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 86400000))
+                .signWith(SignatureAlgorithm.HS256, SECRET)
+                .compact();
+    }
+
+    public String extractEmail(String token) {
+        return Jwts.parser()
+                .setSigningKey(SECRET)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+}
---
+++ auth-service/src/main/java/com/jitlabs/authservice/service/AuthService.java
@@ -0,0 +1,24 @@
+package com.jitlabs.authservice.service;
+
+import com.jitlabs.authservice.config.JwtUtil;
+import com.jitlabs.authservice.entity.User;
+import com.jitlabs.authservice.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service @RequiredArgsConstructor
+public class AuthService {
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
+
+    public String register(User user){
+        repository.findByEmail(user.getEmail()).ifPresent(u -> {
+            throw new RuntimeException("User already exists");
+        });
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        repository.save(user);
+        return "User registered successfully";
+    }
+}
